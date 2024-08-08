3d6864cdb2f2dca02438b67fcc92bda2aa214959
nsamardzic13 <nikola.samardzic1997@gmail.com>
2024-08-08 14:45:47 +0200

    Added error in case the schedule is not valid

--- lambda/main.py
+++ lambda/main.py
@@ -8,9 +8,10 @@ logger.setLevel("INFO")
 def lambda_handler(event,context):
     logger.info('### EVENT')
     logger.info(event)
-    
+
     symbols = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "SOLUSDT"]
     bh = BinanceHelper()
+
     if event.get('schedule') == 'every_20_min':
         prices = bh.get_latest_ticker_prices(symbols)
         bh.insert_json_into_table(
@@ -18,13 +19,15 @@ def lambda_handler(event,context):
             data=prices,
             schema=schemas.ticker_prices_schema
         )
-
-    if event.get('schedule') == '2_times_a_day':
+    elif event.get('schedule') == '2_times_a_day':
         volume = bh.get_ticker_price_change_24hrs(symbols)
         bh.insert_json_into_table(
             table_name="ticker_price_change",
             data=volume,
             schema=schemas.ticker_price_change_schema
         )
+    else:
+        logger.error('Invalid event schedule')
+        raise
 
 # lambda_handler({'schedule': 'every_20_min'}, None)
