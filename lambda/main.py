62583a9039e0b092c447b77bdd8f57408c266086
nsamardzic13 <nikola.samardzic1997@gmail.com>
2024-08-08 12:37:21 +0200

    Testing

--- lambda/main.py
+++ lambda/main.py
@@ -2,9 +2,9 @@ from utils import BinanceHelper
 import schemas
 
 def lambda_handler(event,context):
+    print(event, context)
     symbols = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "SOLUSDT"]
     bh = BinanceHelper(config_path='config.yml')
-    print(event, context)
     if event.get('schedule') == 'every_20_min':
         prices = bh.get_latest_ticker_prices(symbols)
         bh.insert_json_into_table(
