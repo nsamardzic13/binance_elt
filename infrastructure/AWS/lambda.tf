de40b653d5dcbf07d303eaeb0b4afb7d3fa81d4a
nsamardzic13 <nikola.samardzic1997@gmail.com>
2024-08-25 16:22:52 +0200

    Testing

--- infrastructure/AWS/lambda.tf
+++ infrastructure/AWS/lambda.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda" {
   type        = "zip"
-  source_dir  = "../lambda"
-  output_path = "../lambda.zip"
+  source_dir  = "../../lambda"
+  output_path = "../../lambda.zip"
 }
 
 # Define the Lambda function
